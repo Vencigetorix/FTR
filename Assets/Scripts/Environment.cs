2da574df941dba63062a97d68c7790b244fa9f26
Alejandro Escalante <alejandroescalantehdz@gmail.com>
2021-09-22 11:14:18 -0500

    Enviroment

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -9,32 +9,66 @@ public class Environment : MonoBehaviour
     public Color[] color;
     public float temp;
     public float hora;
-    public int clima; //Clima [0:4] 0 : Despejado 1 : Tormenta 
+    public int clima; //Clima [0:5] 0 : Despejado 1 : Nublado 2 : nieve 3 : Tormenta 4 : Tormentafuerte  5 : radioactivo
     public float speedTime; 
     public int day;
     public Light sun;
+    public float visibility;
     void Start()
     {
         day = 0;
         clima = 0;
-        //temp = 19.0f;
+        temp = 14.0f;
         hora = 10.0f;
         speedTime = 1.0f;
     }
+// -2  8 18
+// -6  4 14
+// -10 0 10
+// -14 -4 6
+// -18 -8 2
+// -22 -12 -2
 
     // Update is called once per frame
     void Update()
     {
-        //Light and time
-        //sun.gameObject.transform.Rotate(Time.deltaTime*speedTime, 0, 0, Space.World);
-        
         hora += Time.deltaTime*speedTime;
         if(hora >= 24){
             hora = 0;
             day ++;
+            clima = Random.Range(0,6);
         }
+        if(hora >= 7 && hora <= 19){
+            temp = temp + 1.5f; //day
+        }else{  //Night 
+            if (hora != 0){
+                temp = temp - 1.0f;  
+            }
+            else{
+                switch (clima){
+                    case 0:
+                        temp = 8.0f;
+                        break;
+                    case 1:
+                        temp = 4.0f;
+                        break;
+                    case 2:
+                        temp = 0.0f;
+                        break;
+                    case 3:
+                        temp = -4.0f;
+                        break;
+                    case 4:
+                        temp = -8.0f;
+                        break;
+                    case 5:
+                        temp = -12.0f;
+                        break;
+                }
+            }            
+        }           
     }
-
     //¿Cómo podemos controlar la temperatura copn respecto a la hora y al clima?
     
 }
+
